8598c451586e74251da7594a8eec800e2d95c305
dillona25 <dillon.arnold@allofiber.com>
2025-10-12 19:35:05 -0700

    Added React and Bootstrap Dependencies

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <h1 className="text-warning">Here we are</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
